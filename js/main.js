c5c3173b247fa579f982cd0bad7fb8d4779d68c7
Alessandro <alessandro.piccione.75@gmail.com>
2019-07-19 20:18:34 +0100

    manage ajax request success/error. add function to show/hide error. improve UI

--- js/main.js
+++ js/main.js
@@ -1,19 +1,43 @@
 const dataUrl = "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json"
 
-let ajax = () => new XMLHttpRequest()
+//let ajax = () => new XMLHttpRequest()
+
+let isLoading = false
+
+var __debug
+
+function showError(error) {
+    document.getElementById("errorMessage").innerHTML = error
+    document.getElementById("errorMessage").style.display = ""
+}
+
+let clearError = () => document.getElementById("errorMessage").style.display = "none"
 
 function loadData() {
-   //ajax()
+    if(isLoading) return alert("A previous request is still running")
+    isLoading = true
+
     console.info("start loadData")
 
     $("#loadDataButton").attr("disabled", "");
     
+    clearError()
+
     $.get(dataUrl, 
-        (data) => alert("success"),
-        (error) => alert(error) 
-        )
+        (data, status, xhr) => {
+            clearError()
+            $("#dataTable").css("display", "")
+            $("#dataEmpty").css("display", "none")
+        })
+        .fail( (xhr, error) => { 
+            showError("Failed to load data. " + xhr.statusText)
+            //alert("fail: " + xhr.statusText) /* + (error.message | String(error)) ); __debug = error */ 
+        }) 
+        .always( () => {
+            isLoading = false
+            $("#loadDataButton").removeAttr("disabled")
+
+        })    
 
     console.info("end loadData")
-
-    $("#loadDataButton").removeAttr("disabled");
 }
