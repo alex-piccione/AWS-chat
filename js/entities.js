8af4d9d4b0297d441c177727014120b92ac138db
Alessandro <alessandro.piccione.75@gmail.com>
2019-07-21 13:23:19 +0100

    Call PetStore API and populate table

---
+++ js/entities.js
@@ -0,0 +1,4 @@
+function Pet() {
+    id = (value) =>  if (value) this.id = value else return this.id
+
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const dataUrl = "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json"
+//const dataUrl = "https://codersuk-test.s3-eu-west-1.amazonaws.com/data.json"
+dataUrl = "https://7iv0c9qr3l.execute-api.us-east-1.amazonaws.com/new/pets"
 
 //let ajax = () => new XMLHttpRequest()
 
@@ -13,13 +14,75 @@ function showError(error) {
 
 let clearError = () => document.getElementById("errorMessage").style.display = "none"
 
+
+function loadPets() {
+    if(isLoading) return alert("A previous request is still running")
+    isLoading = true
+
+    console.info("start loadPets")
+
+    $("#loadDataButton").attr("disabled", "");
+    
+    clearError()
+
+    $.get(dataUrl, 
+        (data, status, xhr) => {
+            clearError()
+            $("#dataTable").css("display", "")
+            $("#dataEmpty").css("display", "none")
+
+            showPets(data)
+        })
+        .fail( (xhr, error) => { 
+            showError("Failed to load pets. " + xhr.statusText)
+            //alert("fail: " + xhr.statusText) /* + (error.message | String(error)) ); __debug = error */ 
+        }) 
+        .always( () => {
+            isLoading = false
+            $("#loadDataButton").removeAttr("disabled")
+        })    
+
+    console.info("end loadPets")
+}
+
+function showPets(data) {
+    console.info("showPets")
+    __debug = data
+    
+    // parse the JSON into objects
+    let pets = []
+
+    for (let index = 0; index < data.length; index++) {
+        const element = data[index];
+        let pet = {}
+        pet.id = element.id
+        pet.type = element.type
+        pet.price = element.price
+        pets.push(pet)
+    }
+    //debugger;
+    // populate the table
+    $("#dataTable").css("display", "") // temp
+    let tbody = $("#dataTable tbody")
+    tbody.empty();
+    let table = $("#dataTable table")[0] 
+    
+    pets.forEach(pet => {
+        let row = table.insertRow(-1)
+        let cell = row.insertCell(0).innerText = pet.id
+        row.insertCell(1).innerText = pet.type
+        row.insertCell(2).innerText = pet.price
+    });
+    
+}
+
 function loadData() {
     if(isLoading) return alert("A previous request is still running")
     isLoading = true
 
     console.info("start loadData")
 
-    $("#loadDataButton").attr("disabled", "");
+    $("#loadDataButton").attr("disabled", "")
     
     clearError()
 
